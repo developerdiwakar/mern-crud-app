c934e56912eb20637c9c32e98ce2505ca8549c98
Diwakar Kumar <diwakar.kumar@mpokket.com>
2022-03-11 18:51:21 +0530

    CRUD Operations completed

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -4,7 +4,14 @@ module.exports = app => {
 
     // Create new tutorial
     router.post("/", tutorials.create);
-
+    // Retrieve All Tutorials
+    router.get("/", tutorials.findAll);
+    // Retrieve One tutorial
+    router.get("/:id", tutorials.findOne);
+    // Update tutorial
+    router.put("/:id", tutorials.update);
+    // Delete tutorial
+    router.delete("/:id", tutorials.delete);
     // Set the tutorial routes path
     app.use("/api/tutorials", router);
 }
